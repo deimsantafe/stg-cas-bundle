7d7efb068b4eb1c1a8103f90ef5cfddd6e87248a
Brisa <bbasconcel@santafe.gov.ar>
2026-04-17 13:56:24 -0300

    Se actualiza archivo

--- CasBundle.php
+++ CasBundle.php
@@ -1,9 +1,19 @@
 <?php
 
-namespace Stg\Bundle\CasBundle;
+namespace STG\DEIM\Security\Bundle\CasBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use STG\DEIM\Security\Bundle\CasBundle\DependencyInjection\Security\Factory\CasFactory;
 
 class CasBundle extends Bundle
 {
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+
+        /** @var \Symfony\Bundle\SecurityBundle\DependencyInjection\SecurityExtension $extension */
+        $extension = $container->getExtension('security');
+        $extension->addAuthenticatorFactory(new CasFactory());
+    }
 }
