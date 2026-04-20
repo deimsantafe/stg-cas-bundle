8a9e8ca54565250ba1e6cc90a82fff7337147738
Brisa <bbasconcel@santafe.gov.ar>
2026-04-20 15:35:36 -0300

    Fix namespace y parametros

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,71 +1,37 @@
 <?php
 
-namespace Stg\Bundle\CasBundle\DependencyInjection;
+namespace STG\DEIM\Security\Bundle\CasBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 class Configuration implements ConfigurationInterface
 {
-    const CAS_VERSION_3_0 = '3.0';
-    const CAS_VERSION_2_0 = '2.0';
-    const CAS_VERSION_1_0 = '1.0';
-
     public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('cas');
         $rootNode = $treeBuilder->getRootNode();
         $rootNode
             ->children()
-                ->scalarNode('hostname')
-                    ->defaultValue('https://dsso.santafe.gob.ar/')
-                    ->example('example.org')
-                    ->info('Enter the hostname of the CAS server.')
-                ->end()
-                ->integerNode('port')
-                    ->defaultValue(443)
-                    ->example('443')
-                    ->info('Server cas port')
-                ->end()
                 ->scalarNode('url')
-                    ->defaultValue('/service-auth')
-                    ->example('/service-auth')
-                    ->info('REQUEST_PATH of the CAS server.')
+                    ->isRequired()
+                    ->info('URL base del servidor CAS (e.g. https://dsso.santafe.gob.ar/service-auth)')
                 ->end()
-                ->scalarNode('service_base_url')
-                    ->defaultValue('http://localhost:80')
-                    ->example('http://localhost:80')
-                    ->info('Url del servicio que utiliza cas')
+                ->scalarNode('server')
+                    ->defaultNull()
+                    ->info('URL del servidor CAS para validación (por defecto igual a url)')
                 ->end()
-                ->booleanNode('debug')
-                    ->defaultValue(false)
-                    ->example('true')
-                    ->info('If true log in file.')
+                ->scalarNode('cert')
+                    ->defaultNull()
+                    ->info('Ruta al certificado SSL (opcional)')
                 ->end()
-                ->enumNode('version')
-                    ->values([
-                        self::CAS_VERSION_3_0,
-                        self::CAS_VERSION_2_0,
-                        self::CAS_VERSION_1_0,
-                    ])
-                    ->defaultValue(self::CAS_VERSION_3_0)
-                    ->example('3.0')
-                    ->info('Version of the CAS Server.')
-                ->end()
-                ->scalarNode('user')
+                ->scalarNode('username_attribute')
                     ->defaultValue('cuil')
-                    ->example('cuil')
-                    ->info('Atributo utilizado como identificador')
+                    ->info('Atributo del ticket CAS utilizado como identificador de usuario')
                 ->end()
-                ->scalarNode('logout_redirect')
-                    ->defaultValue('')
-                    ->example('home')
-                    ->info('Url after logout successfull')
-                ->end()
-                ->scalarNode('login_failure')
-                    ->defaultValue('')
-                    ->example('failure_path')
-                    ->info('Url when login failure')
+                ->booleanNode('proxy')
+                    ->defaultFalse()
+                    ->info('Habilitar modo proxy CAS')
                 ->end()
             ->end();
 
